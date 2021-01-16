947ab3e2ffae3a0815140440eae24284392fe9ed
vinaykangralkar <vinaykangralkar9@gmail.com>
2021-01-16 17:39:26 +1100

    Room list

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,30 +1,61 @@
 import React from 'react';
 import RoomsList from './RoomList';
 import RoomsFliter from './RoomFilter';
-import {RoomConsumer} from '../Context';
+import {withRoomConsumer} from '../Context';
 import Loading from './Loading';
 
-export default function RoomContainer() {
-    
-    return (
-        <RoomConsumer>
-            {
-                (value) => {
-                    const {loading, sortedRooms, rooms} = value
+function RoomContainer({context}) {
+    const {loading, sortedRooms, rooms} = context;
 
-                    if(loading){
-                        return <Loading />
-                    }
-                    return (
-                        <div>
-                        Rooms container
-                        <RoomsFliter room={rooms}/>
-                        <RoomsList room={sortedRooms}/>
-                    </div>
-                    )
-                }
-            }
-        </RoomConsumer>
-    );
-    
+    if(loading){
+        return <Loading />
+    }
+    return (
+        <div>
+            
+            <RoomsFliter rooms={rooms}/>
+            <RoomsList rooms={sortedRooms}/>
+        </div>
+    )
 }
+
+export default withRoomConsumer(RoomContainer)
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import RoomsList from './RoomList';
+// import RoomsFliter from './RoomFilter';
+// import {RoomConsumer} from '../Context';
+// import Loading from './Loading';
+
+// export default function RoomContainer() {
+    
+//     return (
+//         <RoomConsumer>
+//             {
+//                 (value) => {
+//                     const {loading, sortedRooms, rooms} = value
+
+//                     if(loading){
+//                         return <Loading />
+//                     }
+//                     return (
+//                         <div>
+//                         Rooms container
+//                         <RoomsFliter room={rooms}/>
+//                         <RoomsList room={sortedRooms}/>
+//                     </div>
+//                     )
+//                 }
+//             }
+//         </RoomConsumer>
+//     );
+    
+// }
--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Room from './Room';
 
-export default function RoomList() {
-    return (
-        <div>
-            Rooms List
+export default function RoomList({rooms}) {
+    console.log({rooms});
+    if(rooms.length === 0){
+        return (
+            <div className='empty-search'>
+                <h3>unfortunately no rooms matched</h3>
+            </div>
+        )
+    }
+
+    return <section className='roomsList'>
+        <div className='roomsList-center'>
+            {
+                rooms.map(item => {
+                    return <Room key={item.id} room={item} />
+                })
+            }
         </div>
-    )
+    </section>
 }
