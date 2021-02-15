f875e97f0c22eb12bbe8e59dd25c1efbc7376560
vinaykangralkar <vinaykangralkar9@gmail.com>
2021-02-16 09:34:44 +1100

    updates

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -6,9 +6,9 @@ import {Link} from 'react-router-dom';
 const Error = () => {
     return(
         <Hero>
-            <Banner title='404' subtitle='Page not found'>
+            {/* <Banner title='404' subtitle='Page not found'>
                 <Link to='/' className='btn-primary'>Return home</Link>
-            </Banner>
+            </Banner> */}
         </Hero>
     );
 }
